4fa5c6d09aacf1cc8f7bbc3ad67cb45af1bf8fd5
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2024-09-08 20:36:02 +0300

    Remake description. Fix minor details in configuration files, add function to check whether application is running inside docker.

--- client/src/main/java/ru/edu/spbstu/client/utils/Conf.java
+++ client/src/main/java/ru/edu/spbstu/client/utils/Conf.java
@@ -1,5 +1,21 @@
 package ru.edu.spbstu.client.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Conf {
-    public static  String ip="http://"+"serverjar";
+
+    public static Boolean isRunningInsideDocker() {
+
+            try (Stream< String > stream =
+                         Files.lines(Paths.get("/proc/1/cgroup"))) {
+                return stream.anyMatch(line -> line.contains("/docker"));
+            } catch (IOException e) {
+                return false;
+            }
+    }
+
+    public static  String ip="http://"+((isRunningInsideDocker())?"serverjar":"localhost");
 }
